b2a6cec003233e285ada8b9c7dcf0f63c0ecb644
Karl Warren Ga-as <kgaas@umindanao.edu.ph>
2023-04-25 14:02:47 +0800

    the admin can now add and edit shortcodes using admin account

--- app/Http/Controllers/Shortcodes/ShortcodesController.php
+++ app/Http/Controllers/Shortcodes/ShortcodesController.php
@@ -13,12 +13,14 @@ use Illuminate\Database\Schema\Blueprint;
 
 class ShortcodesController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
+       
         $shortcodes = ShortcodesModel::orderBy('position', 'asc')
-            ->when (!empty($request['category']) && ($request['category'] != "all") , function ($query) use($request){
-                return $query->where('shortcode_category_id',$request['category']);
+            ->when(!empty($request['category']) && ($request['category'] != "all"), function ($query) use ($request) {
+                return $query->where('shortcode_category_id', $request['category']);
             })
-            ->when (!empty($request['q']) , function ($query) use($request){
+            ->when(!empty($request['q']), function ($query) use ($request) {
                 return $query->Where('name', 'LIKE', '%' . $request['q'] . '%');
             })
             ->get();
@@ -27,13 +29,14 @@ class ShortcodesController extends Controller
         return view('pages.admin.shortcodes.index', compact('shortcodes', 'shortcode_categories'));
     }
 
-    public function wpPluginIndex(){
-        $request= request();
+    public function wpPluginIndex()
+    {
+        $request = request();
         $shortcodes = ShortcodesModel::where('display_on_wp', 1)->orderBy('position', 'asc')
-            ->when (!empty($request['category']) && ($request['category'] != "all") , function ($query) use($request){
-                return $query->where('shortcode_category_id',$request['category']);
+            ->when(!empty($request['category']) && ($request['category'] != "all"), function ($query) use ($request) {
+                return $query->where('shortcode_category_id', $request['category']);
             })
-            ->when (!empty($request['q']) , function ($query) use($request){
+            ->when(!empty($request['q']), function ($query) use ($request) {
                 return $query->Where('name', 'LIKE', '%' . $request['q'] . '%');
             })
             ->get();
@@ -42,96 +45,96 @@ class ShortcodesController extends Controller
         return view('pages.admin.wp-plugins.index', compact('shortcodes', 'shortcode_categories'));
     }
 
-    public function downloadPlugin(){
+    public function downloadPlugin()
+    {
         $file_path = public_path('wp-plugin/agencybuildershortcodes_dev.zip');
         return response()->download($file_path);
     }
 
-    public function create(Request $request){
+    public function create(Request $request)
+    {
         $data = $request->all();
+        
         if (!Schema::hasColumn('businesses', $data['name']) && !ShortcodesModel::where('name', $data['name'])->exists()) {
-            
-            $Position = ShortcodesModel::max('position') + 1; 
-            $name = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '_', $data['name'])));                
-            $shortcode = '[AgencyBuilder_'.strtolower(str_replace(" ","_", $name)).']';
-            
-            $newColumn = strtolower(str_replace(" ","_", $data['name']));
+
+            $Position = ShortcodesModel::max('position') + 1;
+            $name = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '_', $data['name'])));
+            $shortcode = '[AgencyBuilder_' . strtolower(str_replace(" ", "_", $name)) . ']';
+
+            $newColumn = strtolower(str_replace(" ", "_", $data['name']));
 
             try {
-                Schema::table('businesses', function($table) use ($newColumn){
+                Schema::table('businesses', function ($table) use ($newColumn) {
                     $table->string($newColumn)->nullable();
                 });
 
-                Schema::table('user_onlineforms', function($table) use ($newColumn){
+                Schema::table('user_onlineforms', function ($table) use ($newColumn) {
                     $table->boolean($newColumn)->default('0');
                 });
-                
-                Schema::table('pending_orders', function($table) use ($newColumn){
+
+                Schema::table('pending_orders', function ($table) use ($newColumn) {
                     $table->string($newColumn)->nullable();
                 });
                 // $shortcode_category_id = $data['data'],;
                 ShortcodesModel::create(array_merge($data, [
-                    'position' => $Position, 
+                    'position' => $Position,
                     'shortcode' => $shortcode,
-                    'business_column'=> $newColumn, 
-                ] ));
+                    'business_column' => $newColumn,
+                ]));
 
                 return redirect('/admin/shortcodes')->with('message', 'New Shortcode Successfully Added');
             } catch (\Throwable $th) {
                 dd($th->getMessage());
                 return redirect('admin/shortcodes/')->with('error', $th->getMessage());
-            }                
-                       
-    }else{
-        return redirect('/admin/shortcodes')->with('message', $data['name'] . ' already exists in table')->with('exist', '1');
+            }
+        } else {
+            return redirect('/admin/shortcodes')->with('message', $data['name'] . ' already exists in table')->with('exist', '1');
+        }
     }
-}
 
-public function update(Request $request)
-{
-    dd($request->all());
-    $data = request()->validate([
-        'name' => ['required '],
-        'shortcode_category_id' => 'required',
-        'type' => 'required',                  
-        'position' => '',
-        'full' => '',
+    public function update(Request $request)
+    {
+        
+        $data = request()->validate([
+            'name' => ['required '],
+            'shortcode_category_id' => 'required',
+            'type' => 'required',
+            'position' => '',
+            'full' => '',
 
-    ]);
-    $shortcode_id = $request->id;
-    $enable = (request()->get('enable') ? 1 : 0);
-    $full = (request()->get('full') ? 1 : 0);
-    
-    $show_to_dashboard = (request()->get('show_to_dashboard') ? 1 : 0);
-    $display_on_wp = (request()->get('display_on_wp') ? 1 : 0);
-    $required = (request()->get('required') ? 1 : 0);
-    $oldColumn = request('oldColumn');
-    $newColumn  = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '_', $data['name']))); 
-    //$newColumn = strtolower(str_replace(" ","_", $data['name']));
-    $shortcode = '[AgencyBuilder_'.$newColumn.']';   
-    
-    try {
-        // Schema::table('businesses', function($table) use ($oldColumn, $newColumn){
-        //     $table->renameColumn($oldColumn, $newColumn);
-        // });
-        // Schema::table('user_onlineforms', function(Blueprint $table) use ($oldColumn, $newColumn){
-        //     $table->renameColumn($oldColumn, $newColumn);
-        // });
-        ShortcodesModel::findOrFail($shortcode_id)->update(array_merge($data, [
-            'business_column'=> $newColumn, 
-            'shortcode' => $shortcode, 
-            'enable' => $enable,
-            'show_to_dashboard' => $show_to_dashboard,
-            'display_on_wp' => $display_on_wp,
-            'required' => $required,  
-            'full' => $full,
+        ]);
+        $shortcode_id = $request->id;
+        $enable = (request()->get('enable') ? 1 : 0);
+        $full = (request()->get('full') ? 1 : 0);
+
+        $show_to_dashboard = (request()->get('show_to_dashboard') ? 1 : 0);
+        $display_on_wp = (request()->get('display_on_wp') ? 1 : 0);
+        $required = (request()->get('required') ? 1 : 0);
+        $oldColumn = request('oldColumn');
+        $newColumn  = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '_', $data['name'])));
+        //$newColumn = strtolower(str_replace(" ","_", $data['name']));
+        $shortcode = '[AgencyBuilder_' . $newColumn . ']';
+
+        try {
+            // Schema::table('businesses', function($table) use ($oldColumn, $newColumn){
+            //     $table->renameColumn($oldColumn, $newColumn);
+            // });
+            // Schema::table('user_onlineforms', function(Blueprint $table) use ($oldColumn, $newColumn){
+            //     $table->renameColumn($oldColumn, $newColumn);
+            // });
+            ShortcodesModel::findOrFail($shortcode_id)->update(array_merge($data, [
+                'business_column' => $newColumn,
+                'shortcode' => $shortcode,
+                'enable' => $enable,
+                'show_to_dashboard' => $show_to_dashboard,
+                'display_on_wp' => $display_on_wp,
+                'required' => $required,
+                'full' => $full,
             ]));
 
             return redirect('/admin/shortcodes')->with('message', 'Shortcode Successfully updated');
-            
         } catch (\Exception $e) {
             return redirect('/admin/shortcodes')->with('error', $e);
         }
+    }
 }
-}
-
